6a0e39f43f8401bb0cce6f4aaba5cdea73e33506
José Pedro Teixeira Monteiro <jpedrotm@github.com>
2017-02-18 16:26:41 +0000

    Lab1 completed need to fix a bug

---
+++ Lab1/src/Client.java
@@ -0,0 +1,101 @@
+import java.io.IOException;
+import java.net.*;
+
+/**
+ * Created by jpedrotm on 17-02-2017.
+ */
+public class Client{
+
+    final public int BUF_SIZE=512;
+
+    private DatagramPacket packet;
+    private DatagramSocket socket;
+    private InetAddress address;
+    private String operation;
+    private String plate;
+    private String owner;
+    private String port;
+
+    public static void main(String[] args) throws IOException {
+
+        /*if(args.length!=4 || args.length!=5)
+        {
+            System.out.println("Wrong number of arguments.");
+        }*/
+
+        try{
+
+            Client client=new Client(args[0],args[1],args[2],args);
+            client.start();
+
+        }catch(SocketException e){
+            System.out.println("Error opening server.\n");
+        }
+
+
+
+
+    }
+
+    public Client(String hostName,String port,String opr,String[] args) throws SocketException, UnknownHostException {
+
+        System.out.println("PORT: "+port);
+
+        this.socket=new DatagramSocket();
+        this.address=InetAddress.getByName(hostName);
+        this.operation=opr;
+        this.plate=args[3];
+        this.port=port;
+
+        if(this.operation.equals("REGISTER")){
+            this.owner=args[4];
+        }else{
+            this.owner=null;
+        }
+
+    }
+
+    public void start() throws IOException {
+
+
+        String message;
+        byte[] sbuf;
+
+        if(this.operation.equals("REGISTER")){
+
+            message=this.operation+" "+this.plate+" "+this.owner;
+            sbuf=message.getBytes();
+            packet=new DatagramPacket(sbuf,sbuf.length,this.address,Integer.parseInt(this.port));
+            this.socket.send(packet);
+
+
+        }else if(this.operation.equals("LOOKUP")){
+
+            message=this.operation+" "+this.plate;
+            sbuf=message.getBytes();
+            packet=new DatagramPacket(sbuf,sbuf.length,this.address,Integer.parseInt(this.port));
+            this.socket.send(packet);
+
+        }else{
+            System.out.println("Operation not available.");
+        }
+
+        try {
+            this.waitAnswer();
+        }
+        catch(IOException e){
+            System.out.println("Server answer not received.");
+        }
+
+    }
+
+    private void waitAnswer() throws IOException {
+
+        packet=new DatagramPacket(new byte[BUF_SIZE],BUF_SIZE);
+        socket.receive(packet);
+        String messageReceived=new String(packet.getData());
+        System.out.println("Server message: "+messageReceived);
+
+    }
+
+}
---
+++ Lab1/src/Server.java
@@ -0,0 +1,131 @@
+/**
+ * Created by jpedrotm on 17-02-2017.
+ */
+
+import java.io.IOException;
+import java.net.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+
+public class Server {
+
+    final public int BUF_SIZE=512;
+    final public int TYPE=0;
+    final public int PLATE=1;
+    final public int OWNER=2;
+
+    private HashMap<String,String> database;
+    private DatagramSocket socket;
+    public static void main(String[] args) throws IOException {
+
+        if(args.length!=1)
+        {
+            System.out.println("Usage: Wrong number of arguments.\n");
+        }
+
+        try{
+
+            Server server=new Server(args[0]);
+            server.start();
+
+        }catch(SocketException e){
+            System.out.println("Error opening server.\n");
+        }
+
+    }
+
+    public Server(String port) throws SocketException{
+
+        this.database=new HashMap<>();
+        this.socket=new DatagramSocket(Integer.parseInt(port));
+
+
+    }
+
+    public void start() throws IOException {
+
+        while(true){
+            DatagramPacket packet=new DatagramPacket(new byte[BUF_SIZE],BUF_SIZE);
+
+            socket.receive(packet); //só sai do receive quando recebe alguma informação
+            String buffer=new String(packet.getData());
+            String[] tokens=buffer.split("\\s");
+
+            System.out.println("COMMAND: "+buffer);
+
+            if(tokens[TYPE].equals("REGISTER") && tokens.length==3)
+            {
+                int num=this.register(tokens[PLATE],tokens[OWNER]);
+                String result=Integer.toString(num);
+
+                try{
+                    this.sendAnswer(result,packet);
+                }
+                catch(SocketException e){
+                    System.out.println("Server response failed.");
+                }
+
+            }else if(tokens[TYPE].equals("LOOKUP") && tokens.length==2)
+            {
+                System.out.println("ENTERED LOOKUP");
+                String name=this.lookup(tokens[PLATE]);
+
+                try{
+                    this.sendAnswer(name,packet);
+                }
+                catch(SocketException e){
+                    System.out.println("Server response failed.");
+                }
+
+            }
+            else
+            {
+                System.out.println("Not a valid command.\n");
+            }
+
+        }
+
+    }
+
+    private void sendAnswer(String answer,DatagramPacket packet) throws IOException {
+
+        int port=packet.getPort();
+        InetAddress address=packet.getAddress();
+        byte sbuf[]=answer.getBytes();
+        packet=new DatagramPacket(sbuf,sbuf.length,address,port);
+        socket.send(packet);
+
+    }
+
+    private int register(String plate,String owner){
+
+        System.out.println("PLATE: "+plate+"\n"+"Owner:"+owner);
+
+        if(database.get(plate)==null)
+        {
+            database.put(plate,owner);
+            return database.size();
+        }
+
+        System.out.println("Plate already registed.\n");
+
+        return -1;
+    }
+
+    private String lookup(String plate){
+
+        System.out.println("PLATE: "+plate);
+
+        String name=database.get(plate);
+
+        if(name!=null)
+        {
+            return name;
+        }
+        else{
+            return "NOT_FOUND";
+        }
+    }
+
+}
