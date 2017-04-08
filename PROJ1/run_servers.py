755f937a0b4372fbb49e50f15c8f0c8f58e937a4
João Barbosa <bmpj13@hotmail.com>
2017-04-08 18:49:02 +0100

    script now runs RMI aswell

--- PROJ1/run_servers.py
+++ PROJ1/run_servers.py
@@ -23,12 +23,30 @@ if len(argv) < 9:
     print(help)
     exit()
 
+
 version = argv[1]
 channels = " ".join(argv[2:8])
 
-# ACIONAR RMIREGISTRY
 
+# Start RMI
+p1 = subprocess.Popen(['ps', '-ef'], stdout=subprocess.PIPE)
+p2 = subprocess.Popen(['grep', 'rmiregistry'], stdin=p1.stdout, stdout=subprocess.PIPE)
+p3 = subprocess.Popen(['awk', "{ print $2 }"], stdin=p2.stdout, stdout=subprocess.PIPE)
+output, err = p3.communicate()
+
+entries = output.count('\n')
+
+if (entries <= 1):
+    os.chdir('bin/')
+    rmi_process = subprocess.Popen('rmiregistry')
+    os.chdir('..')
+
+
+# Start servers
 for item1, item2 in grouper(2, argv[8::]):
     cmd = " ".join(['java -cp bin/ server.Server', item1, channels])
     process = subprocess.Popen(['gnome-terminal', '-e', cmd], stdout=subprocess.PIPE)
     output, error = process.communicate();
+
+# Close running RMIs
+# ps -ef | grep rmiregistry | awk '{ print $2 }' | xargs kill
