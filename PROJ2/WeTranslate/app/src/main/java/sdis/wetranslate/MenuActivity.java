25b39c4ae61dc95b1fcc9ee9e3482103cdc7fefe
José Pedro Teixeira Monteiro <j.pedroteixeira.monteiro@gmail.com>
2017-05-19 15:57:51 +0100

    log out progress

--- PROJ2/WeTranslate/app/src/main/java/sdis/wetranslate/MenuActivity.java
+++ PROJ2/WeTranslate/app/src/main/java/sdis/wetranslate/MenuActivity.java
@@ -76,10 +76,6 @@ public class MenuActivity extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
